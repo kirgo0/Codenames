024268dccb7067465e843ef4bedff534a2123186
kirgo <kirgotop@gmail.com>
2024-11-02 20:01:54 +0200

    Relocated Icons folder

---
+++ CodenamesWeb/Components/Icons/Icon.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace CodenamesWeb.Components.Icons
+{
+    public abstract class Icon : IconBase
+    {
+        private protected sealed override RenderFragment Body => BuildRenderTree;
+
+        // Allow content to be provided by a .razor file but without 
+        // overriding the content of the base class
+        protected new virtual void BuildRenderTree(RenderTreeBuilder builder)
+        {
+        }
+    }
+}
