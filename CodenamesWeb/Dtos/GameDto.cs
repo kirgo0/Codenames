55feabcbff51c42571a46bcde326adf0ae6fffe5
kirgo <kirgotop@gmail.com>
2024-11-12 21:37:50 +0200

    Made a component of the game lobby page & moved it inside the games page

---
+++ CodenamesWeb/Dtos/GameDto.cs
@@ -0,0 +1,22 @@
+﻿namespace CodenamesWeb.Dtos
+{
+    public class GameDto
+    {
+        public int Id { get; set; }
+        public int PlayersCount { get; set; }
+        public int MaxPlayersCount { get; set; } = 12;
+
+        public GameDto(int id, int playersCount, int maxPlayersCount)
+        {
+            Id = id;
+            PlayersCount = playersCount;
+            MaxPlayersCount = maxPlayersCount;
+        }
+        public GameDto(int id, int playersCount)
+        {
+            Id = id;
+            PlayersCount = playersCount;
+        }
+
+    }
+}
