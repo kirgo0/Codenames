260e357d34d0bfef981ff3eb9fcff9cb48f3864d
kirgo <kirgotop@gmail.com>
2024-11-13 19:48:47 +0200

    Implemented player configuration menu

---
+++ CodenamesWeb/wwwroot/js/script.js
@@ -0,0 +1,6 @@
+﻿function adjustHrWidth() {
+    input = document.getElementById('dynamicNameInput');
+    hr = document.getElementById('dynamicNameHr');
+    input.style.width = input.value.length + 1 + "ch";
+    hr.style.width = input.value.length + 1 + "ch";
+}
