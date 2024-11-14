b12eebe6941f81faa4691484a41046aa2070335a
kirgo <kirgotop@gmail.com>
2024-11-14 19:02:29 +0200

    Implemented game list component

--- CodenamesWeb/wwwroot/js/script.js
+++ CodenamesWeb/wwwroot/js/script.js
@@ -4,3 +4,10 @@
     input.style.width = input.value.length + 1 + "ch";
     hr.style.width = input.value.length + 1 + "ch";
 }
+
+function calculateLastRowHeight() {
+    row = document.getElementById('last-row');
+    table = document.getElementById('game-list-table');
+    console.log(table.parentElement.clientHeight);
+    row.style.height = table.parentElement.clientHeight - table.clientHeight + "px";
+}
